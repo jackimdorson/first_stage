58a1ab3dcab5ef0a3e067a2f9782172d04145111
jack <jackim0627@gmail.com>
2024-04-20 06:01:51 +0800

    goodnight

--- week3/week3-task2.py
+++ week3/week3-task2.py
@@ -1,34 +1,59 @@
-import urllib.request as req
+import urllib.request as urllib
+import bs4
 
-def getData(src_ptt_json):
-    # src_ptt_json ="https://www.ptt.cc/bbs/Lottery/index.html"
+data_ls = []
+def getData(src):
 
-    # 建立一個Request物件, 附加Headers訊息
-    request = req.Request(src_ptt_json, headers={
+    request = urllib.Request(src, headers={
         "cookie": "over18=1",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
     })
-    with req.urlopen(request) as res_ptt_json:
-        read_ptt_json = res_ptt_json.read().decode("utf-8")
+    with urllib.urlopen(request) as open_ptt_html:
+        read_ptt_html = open_ptt_html.read().decode("utf-8")
 
-    import bs4
-    root = bs4.BeautifulSoup(read_ptt_json, "html.parser")
-    titles = root.find_all("div", class_="title")
-    for title in titles:
-        if title.a:
-            print(title.a.string)
-        else:
-            print(title.string)
+        root = bs4.BeautifulSoup(read_ptt_html, "html.parser")
+        rents = root.find_all("div", class_="r-ent")
+        for rent in rents:
+            has_a = rent.select_one(".title > a")
+            has_span = rent.select_one(".nrec > span")
+            if has_a:
+                data_ls.append(has_a.string)
+                if has_span:
+                    data_ls.append(has_span.string)
+                else:
+                    data_ls.append(0)
+                gethref = has_a.get("href")
+                href = "https://www.ptt.cc"+gethref
+                href_req = urllib.Request(href, headers={
+                    "cookie": "over18=1",
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+                })
+                with urllib.urlopen(href_req) as open_click_html:
+                    read_click_html = open_click_html.read().decode("utf-8")
+
+                    root2 = bs4.BeautifulSoup(read_click_html, "html.parser")
+                    times = root2.select_one("#main-content div:nth-of-type(4) > .article-meta-value")
+                    if times:
+                        data_ls.append(times.get_text())
+                    else:
+                        data_ls.append("")
+
     nextLink=root.find("a", string="‹ 上頁")
     return nextLink["href"]
 
 
-url = "https://www.ptt.cc/bbs/Gossiping/index.html"
-
+src = "https://www.ptt.cc/bbs/Lottery/index.html"
 count = 0
-while count<3:
-    url = "https://www.ptt.cc"+getData(url)
-    count +=1
+while count < 3:
+    src = "https://www.ptt.cc" + getData(src)
+    count += 1
 
 
-
+with open("article.csv", "w", encoding="utf-8") as article_csv:
+    i = 1
+    for data in data_ls:
+        if i%3 == 0:
+            article_csv.write(f"{str(data)}\n")
+        else:
+            article_csv.write(f"{str(data)},")
+        i += 1
