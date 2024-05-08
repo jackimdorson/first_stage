4c6ea49c37193e0cf66f2e2869762c9b8855590e
jack <jackim0627@gmail.com>
2024-05-08 23:34:58 +0800

    5.8

--- week6/static/week6.js
+++ week6/static/week6.js
@@ -1,37 +1,45 @@
 const fullname = document.getElementById("fullname");
 const accountUp = document.getElementById("account-up");
 const pswUp = document.getElementById("psw-up");
+const accountIn = document.getElementById("account-in");
+const pswIn = document.getElementById("psw-in");
+const signUp = document.getElementById("signup");
+const signIn = document.getElementById("signin");
+
 
 
 function change2HalfChar(args){
     args.addEventListener("input", function(){
-        args.value = args.value.replace(/[Ａ-Ｚａ-ｚ０-９！＠＃＄＾＊（）＿]/g, function(str){    //參考國稅局, 特殊符號僅允許!@#$^*()
+        args.value = args.value.replace(/[Ａ-Ｚａ-ｚ０-９〜！＠＃＄＾＊（）＿]/g, function(str){    //參考國稅局, 特殊符號僅允許!@#$^*()
             return String.fromCharCode(str.charCodeAt(0) - 0xFEE0);                          //リアルタイムで 全角→半角
         })
-        args.value = args.value.replace(/[^A-Za-z0-9!@#$^*()_]/g, '');                       //上述以外全都不讓User輸入
+        args.value = args.value.replace(/[^A-Za-z0-9~!@#$^*()_]/g, '');                       //上述以外全都不讓User輸入
     })
 }
 change2HalfChar(fullname);
 change2HalfChar(accountUp);
 change2HalfChar(pswUp);
+change2HalfChar(accountIn);
+change2HalfChar(pswIn);
 
 
-
-document.addEventListener("DOMContentLoaded", function(){
-    signup.addEventListener("submit", function(event){
-        const checkName = document.getElementById("fullname");
-        const checkAccount = document.getElementById("account-up");
-        const checkPsw = document.getElementById("psw-up");
-        if(!checkName.value || !checkAccount.value || !checkPsw.value){
-            event.preventDefault();
-            alert("請輸入帳號、密碼");
-        }
+// function checkBlank(formName, input1, input2, input3=false){
+function checkBlank(formName, input1, input2, input3=null){
+    document.addEventListener("DOMContentLoaded", function(){
+        formName.addEventListener("submit", function(event){
+            if(!input1.value || !input2.value || (input3 && !input3.value)){
+                event.preventDefault();
+                alert("請輸入帳號、密碼");
+            }
+        })
     })
-})
+}
+checkBlank(signUp, fullname, accountUp, pswUp);
+checkBlank(signIn, accountIn, pswIn);
+
 
 
 const showPswUp = document.getElementById("show-psw-up")
-const pswIn = document.getElementById("psw-in");
 const showPswIn = document.getElementById("show-psw-in")
 
 function showPsw(psw, img){
@@ -49,12 +57,21 @@ showPsw(pswUp, showPswUp);
 showPsw(pswIn, showPswIn);
 
 
-const accordion = document.getElementById("accordion");
+const accordion = document.querySelector(".accordion");
 accordion.addEventListener("click", function(){
-    if(accordion.classList === "active"){
-        accordion.classList.remove("active");
-        accordion.nextElementSibling.classList.remove("open");
-    }
-    accordion.classList.toggle("active");
-    accordion.nextElementSibling.classList.toggle("open");
+    accordion.classList.toggle("active-accordion");
+    accordion.nextElementSibling.classList.toggle("open-accordion");
+})
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.addEventListener("submit", function(event){
+        if(event.target.classList.contains("delete_form")){
+            const check = confirm("確定要刪除?");
+            if (!check){        // user按Yes=T, 按No=F
+                event.preventDefault();
+            }
+        }
+    })
 })
